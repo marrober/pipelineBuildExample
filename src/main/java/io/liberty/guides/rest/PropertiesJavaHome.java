c1c0171e5892b04091f7790435096f0e5f674b87
Mark Roberts <marrober@redhat.com>
2021-03-10 15:36:19 +0000

    Update PropertiesJavaHome.java

--- src/main/java/io/liberty/guides/rest/PropertiesJavaHome.java
+++ src/main/java/io/liberty/guides/rest/PropertiesJavaHome.java
@@ -10,13 +10,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+// oc get route liberty-rest-route -o jsonpath='{"http://"}{.spec.host}{"/System/propertiesJavaHome"}'
+
 @Path("propertiesJavaHome")
 public class PropertiesJavaHome {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getProperties() {
-       String javaHome = "Java Home --> " + System.getProperty("java.home");
+       String javaHome = "Java Home ++> " + System.getProperty("java.home");
 
        return(javaHome);
     }
