5ea05d07abc19c15eaa2c26764b9d4389ad9b457
Mark Roberts <marrober@redhat.com>
2021-08-11 10:06:41 +0100

    Completion of initial work with image and resource scanning

--- build/create-tasks.sh
+++ build/create-tasks.sh
@@ -1,13 +1,13 @@
 oc create --save-config=true -f image-streams/imageStream-liberty-rest-app.yaml
-oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml
+# oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml
 oc create --save-config=true -f resources/imageStreamResource-intermediate.yaml
 oc create --save-config=true -f resources/imageStreamResource-liberty-rest-app.yaml
 oc create --save-config=true -f resources/sourceCode-GitResource.yaml
 oc create --save-config=true -f tasks/build.yaml
-oc create --save-config=true -f tasks/clearBuildahRepo.yaml
 oc create --save-config=true -f tasks/clearResources.yaml
 oc create --save-config=true -f tasks/createRuntimeImage.yaml
-oc create --save-config=true -f tasks/ocProcessDeploymentTemplate.yaml
+oc create --save-config=true -f tasks/oc-deploy-application.yaml
 oc create --save-config=true -f tasks/pushImageToQuay.yaml
 oc create --save-config=true -f tasks/acs-deployment-check.yaml
+oc create --save-config=true -f tasks/acs-image-check.yaml
 oc create --save-config=true -f pipelines/pipeline.yaml
