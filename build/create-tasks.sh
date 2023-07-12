613667ee2e53d16c32830e4cf82c4ec86d7bb880
Mark Roberts <marrober@redhat.com>
2023-07-12 11:51:10 +0100

    Updates

--- build/create-tasks.sh
+++ build/create-tasks.sh
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 oc create --save-config=true -f image-streams/imageStream-liberty-rest-app.yaml 
 oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml 
 oc create --save-config=true -f resources/imageStreamResource-intermediate.yaml 
@@ -11,3 +12,12 @@ oc create --save-config=true -f tasks/generate-runtime-image-dockerfile.yaml
 oc create --save-config=true -f tasks/ocProcessDeploymentTemplate.yaml 
 oc create --save-config=true -f tasks/pushImageToQuay.yaml
 oc create --save-config=true -f pipelines/pipeline.yaml
+=======
+oc apply -f tasks/build.yaml
+oc apply -f tasks/clearBuildahRepo.yaml
+oc apply -f tasks/clearResources.yaml
+oc apply -f tasks/createRuntimeImage.yaml
+oc apply -f tasks/ocDeployApplication.yaml
+oc apply -f tasks/pushImageToQuay.yaml
+oc apply -f pipelines/pipeline.yaml
+>>>>>>> Stashed changes
