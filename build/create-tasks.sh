3245c1f0854baf46efb5439dcd12020f9f55fee5
Mark Roberts <marrober@redhat.com>
2023-07-13 17:43:23 +0100

    updates

--- build/create-tasks.sh
+++ build/create-tasks.sh
@@ -1,6 +1,5 @@
 oc apply -f image-streams/imageStream-liberty-rest-app.yaml
 oc apply -f tasks/build.yaml
-oc apply -f tasks/clearBuildahRepo.yaml
 oc apply -f tasks/clearResources.yaml
 oc apply -f tasks/createRuntimeImage.yaml
 oc apply -f tasks/ocDeployApplication.yaml
