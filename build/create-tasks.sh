20b11c012504b0293eedbc0bdf51591c39a86500
Mark Roberts <marrober@redhat.com>
2023-07-14 10:05:38 +0100

    update

--- build/create-tasks.sh
+++ build/create-tasks.sh
@@ -1,23 +1,7 @@
-<<<<<<< Updated upstream
-oc create --save-config=true -f image-streams/imageStream-liberty-rest-app.yaml 
-oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml 
-oc create --save-config=true -f resources/imageStreamResource-intermediate.yaml 
-oc create --save-config=true -f resources/imageStreamResource-liberty-rest-app.yaml 
-oc create --save-config=true -f resources/sourceCode-GitResource.yaml 
-oc create --save-config=true -f tasks/build.yaml 
-oc create --save-config=true -f tasks/clearBuildahRepo.yaml 
-oc create --save-config=true -f tasks/clearResources.yaml 
-oc create --save-config=true -f tasks/createRuntimeImage.yaml 
-oc create --save-config=true -f tasks/generate-runtime-image-dockerfile.yaml
-oc create --save-config=true -f tasks/ocProcessDeploymentTemplate.yaml 
-oc create --save-config=true -f tasks/pushImageToQuay.yaml
-oc create --save-config=true -f pipelines/pipeline.yaml
-=======
+oc apply -f image-streams/imageStream-liberty-rest-app.yaml
 oc apply -f tasks/build.yaml
-oc apply -f tasks/clearBuildahRepo.yaml
 oc apply -f tasks/clearResources.yaml
 oc apply -f tasks/createRuntimeImage.yaml
 oc apply -f tasks/ocDeployApplication.yaml
 oc apply -f tasks/pushImageToQuay.yaml
 oc apply -f pipelines/pipeline.yaml
->>>>>>> Stashed changes
